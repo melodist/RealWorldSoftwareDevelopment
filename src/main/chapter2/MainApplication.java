28ecaf97c741618c5383c4c8c357196430e37dd6
MELT <melt.ep00@gmail.com>
2021-05-23 19:22:04 +0900

    Cohesion and coupling Commit

--- src/main/chapter2/MainApplication.java
+++ src/main/chapter2/MainApplication.java
@@ -1,16 +1,2 @@
-package main.chapter2;
-
-import java.io.IOException;
-
-public class MainApplication {
-
-    public static void main(String... args) throws IOException {
-        final BankStatementAnalyzer2 bankStatementAnalyzer
-                = new BankStatementAnalyzer2();
-
-        final BankStatementParser bankStatementParser
-                = new BankStatementCSVParser2();
-
-        bankStatementAnalyzer.analyze(args[0], bankStatementParser);
-    }
+package main.chapter2;public class MainApplication {
 }
